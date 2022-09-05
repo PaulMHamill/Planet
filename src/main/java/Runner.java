25321b3e73a43ee0d6c68c1cd99f0e4beb42f618
paulhamill <enquiries@nestboxproperty.co.uk>
2022-09-05 14:19:47 +0100

    first commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        Planet planet = new planet("Mars", 908973);
+        System.out.println(planet.name is planet.getSize());
+
+
+    }
+}
